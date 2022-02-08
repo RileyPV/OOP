7c516b0068617dada2be1e069324fe78cfbf578a
Riley Richards <riley_richards1@baylor.edu>
2022-02-07 21:42:35 -0600

    OOP Class Exercise

---
+++ Fly.py
@@ -0,0 +1,10 @@
+import InsectClass as i
+
+
+def main():
+
+    my_insect = i.insect
+
+    print("The length of flight is:", my_insect.flight_length())
+
+main()
---
+++ InsectClass.py
@@ -0,0 +1,14 @@
+import random
+
+
+class insect:
+
+    def __init__(self):
+        self.attribues = {"Wings" : 2,
+                        "Legs" : 4}
+
+
+    def flight_length():
+        
+        length=random.randint(1, 10)
+        return length
