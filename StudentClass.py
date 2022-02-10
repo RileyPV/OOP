bace352e2f34680c87d8a256bd9b75187a8d6a82
Riley Richards <riley_richards1@baylor.edu>
2022-02-09 18:36:08 -0600

    student changes

---
+++ StudentClass.py
@@ -0,0 +1,23 @@
+from datetime import date
+from xml.dom import registerDOMImplementation
+
+
+class Student:
+
+    def __inti__(self):
+        self.__studentID
+        self.__name
+        self.__date_of_birth
+        self.__classification = ["f","s","jr","sr"]
+
+    def age():
+        age = 0
+
+    def registration():
+        register=0
+
+    def get_age(self):
+        return self.__age
+
+    def get_registration(self):
+        return self.__registration
