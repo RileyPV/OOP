7cb5af556ef05aa8ed29ced19bf1fa0c1e3d9ecc
Riley Richards <riley_richards1@baylor.edu>
2022-02-16 12:01:39 -0600

    More work on HW: Cellphone and Student

--- CellPhone.py
+++ CellPhone.py
@@ -3,5 +3,6 @@ import CellPhoneClass as cp
 def main():
     my_cellphone = cp.CellPhone("Apple", 11, 900)
 
-    
+    print(my_cellphone.get_manufact(),my_cellphone.get_model(),my_cellphone.get_retail_price())
+
 main()
