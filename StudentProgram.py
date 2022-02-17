0400b71354a6c20dde66cc6b4b696dd1da760434
Riley Richards <riley_richards1@baylor.edu>
2022-02-16 21:14:05 -0600

    Made changes to student program to calc age

--- StudentProgram.py
+++ StudentProgram.py
@@ -4,14 +4,16 @@ import datetime
 
 
 def main():
-    my_student = sc.Student(892021116, "Riley Richards", 1996, "Sr")
+    my_student = sc.Student(892021116, "Riley Richards", 10/12/1996, "Sr")
     today = (datetime.date.today().year)
-    print(today)
-    year = my_student.get_date_of_birth()
-    print(year)
+    #print(today)
+    #year = my_student.get_date_of_birth()
+   # print(year)
+    #dob = int(dob.split('/')[2])
+    age = today - 1996
+    print (age)
     
-    
-    print("The Student is:", my_student.get_age(),"years old.")
+    #print("The Student is:", my_student.get_age(),"years old.")
 
     if my_student.get_classification() == "Sr":
         print("11/1 thru 11/3")
